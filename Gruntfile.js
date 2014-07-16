3f192ba832ccb5c718ee3169ce51ade021972405
Piotr Kierzniewski <kierzniak@gmail.com>
2014-07-16 11:19:08 +0200

    Change indent in js files from 2 to 4 spaces

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,215 +17,215 @@
 
 module.exports = function(grunt) {
 
-  grunt.loadNpmTasks('assemble');
-  grunt.loadNpmTasks('grunt-contrib-compass');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-htmlmin');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-jsbeautifier');
-  grunt.loadNpmTasks('grunt-sync');
+    grunt.loadNpmTasks('assemble');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-jsbeautifier');
+    grunt.loadNpmTasks('grunt-sync');
 
-  // Time how long tasks take. Can help when optimizing build times
-  require('time-grunt')(grunt);
+    // Time how long tasks take. Can help when optimizing build times
+    require('time-grunt')(grunt);
 
-  // Project configuration.
-  grunt.initConfig({
+    // Project configuration.
+    grunt.initConfig({
 
-    config: {
-      src: 'src',
-      dist: 'dist'
-    },
-
-    watch: {
-      assemble: {
-        files: ['<%= config.src %>/{content,data,templates,partials}/**/*.{md,hbs,yml}'],
-        tasks: ['assemble', 'htmlmin:dist']
-      },
-      js: {
-        files: [
-          '<%= config.src %>/js/**/*.js',
-          '!<%= config.src %>/js/lib/**/*.js',
-          'Gruntfile.js'
-        ],
-        tasks: ['jsbeautifier', 'jshint'],
-        options: {
-          livereload: true
-        }
-      },
-      gruntfile: {
-        files: ['Gruntfile.js']
-      },
-      compass: {
-        files: [
-          '<%= config.src %>/scss/**/*.{scss,sass}',
-          '<%= config.src %>/scss/modules/**/*.{scss,sass}',
-          '<%= config.src %>/vendor/**/*.{scss,sass}'
-        ],
-        tasks: ['compass:server'],
-        options: {
-          livereload: true
-        }
-      },
-
-      // When there will be changes in img catalogue execute task sync:server
-      sync: {
-        files: ['<%= config.src %>/{img,vendor,js}/**/*'],
-        tasks: ['sync:img', 'sync:js', 'sync:vendor']
-      },
-      livereload: {
-        options: {
-          livereload: '<%= connect.options.livereload %>'
+        config: {
+            src: 'src',
+            dist: 'dist'
         },
-        files: [
-          '<%= config.dist %>/**/*.html',
-          '<%= config.dist %>/**/*.css',
-          '<%= config.dist %>/**/*.js',
-          '<%= config.dist %>/**/*.{png,jpg,jpeg,gif,webp,svg}'
-        ]
-      }
-    },
 
-    // Compiles Sass to CSS and generates necessary files if requested
-    compass: {
-      options: {
-        importPath: [
-          '<%= config.src %>/vendor/kicss/scss',
-          '<%= config.src %>/vendor/normalize.scss'
-        ]
-      },
-      server: {
-        options: { // Target options
-          sourcemap: true,
-          sassDir: '<%= config.src %>/scss',
-          cssDir: '<%= config.dist %>/css',
-          imagesDir: '<%= config.src %>/img',
-          httpGeneratedImagesPath: '../img',
-          environment: 'development'
-        }
-      }
-    },
+        watch: {
+            assemble: {
+                files: ['<%= config.src %>/{content,data,templates,partials}/**/*.{md,hbs,yml}'],
+                tasks: ['assemble', 'htmlmin:dist']
+            },
+            js: {
+                files: [
+                    '<%= config.src %>/js/**/*.js',
+                    '!<%= config.src %>/js/lib/**/*.js',
+                    'Gruntfile.js'
+                ],
+                tasks: ['jsbeautifier', 'jshint'],
+                options: {
+                    livereload: true
+                }
+            },
+            gruntfile: {
+                files: ['Gruntfile.js']
+            },
+            compass: {
+                files: [
+                    '<%= config.src %>/scss/**/*.{scss,sass}',
+                    '<%= config.src %>/scss/modules/**/*.{scss,sass}',
+                    '<%= config.src %>/vendor/**/*.{scss,sass}'
+                ],
+                tasks: ['compass:server'],
+                options: {
+                    livereload: true
+                }
+            },
 
-    connect: {
-      options: {
-        port: 9000,
-        livereload: 35729,
-        // change this to '0.0.0.0' to access the server from outside
-        hostname: 'localhost'
-      },
-      livereload: {
-        options: {
-          open: true,
-          base: [
-            '<%= config.dist %>'
-          ]
-        }
-      }
-    },
-
-    assemble: {
-      pages: {
-        options: {
-          flatten: true,
-          assets: '<%= config.dist %>',
-          layout: '<%= config.src %>/templates/layouts/default.hbs',
-          data: '<%= config.src %>/data/*.{json,yml}',
-          partials: '<%= config.src %>/templates/partials/*.hbs',
-          plugins: ['assemble-contrib-permalinks', 'assemble-contrib-sitemap'],
+            // When there will be changes in img catalogue execute task sync:server
+            sync: {
+                files: ['<%= config.src %>/{img,vendor,js}/**/*'],
+                tasks: ['sync:img', 'sync:js', 'sync:vendor']
+            },
+            livereload: {
+                options: {
+                    livereload: '<%= connect.options.livereload %>'
+                },
+                files: [
+                    '<%= config.dist %>/**/*.html',
+                    '<%= config.dist %>/**/*.css',
+                    '<%= config.dist %>/**/*.js',
+                    '<%= config.dist %>/**/*.{png,jpg,jpeg,gif,webp,svg}'
+                ]
+            }
         },
-        files: {
-          '<%= config.dist %>/': ['<%= config.src %>/templates/pages/*.hbs']
-        }
-      }
-    },
 
-    htmlmin: { // Task
-      dist: { // Target
-        options: { // Target options
-          removeComments: true,
-          collapseWhitespace: true
+        // Compiles Sass to CSS and generates necessary files if requested
+        compass: {
+            options: {
+                importPath: [
+                    '<%= config.src %>/vendor/kicss/scss',
+                    '<%= config.src %>/vendor/normalize.scss'
+                ]
+            },
+            server: {
+                options: { // Target options
+                    sourcemap: true,
+                    sassDir: '<%= config.src %>/scss',
+                    cssDir: '<%= config.dist %>/css',
+                    imagesDir: '<%= config.src %>/img',
+                    httpGeneratedImagesPath: '../img',
+                    environment: 'development'
+                }
+            }
         },
-        files: { // Dictionary of files
-          'dist/index.html': 'dist/index.html',
-        }
-      },
-    },
 
-    // Task for syncing content of img catalogue content to dest
-    sync: {
-      img: {
-        expand: true,
-        cwd: '<%= config.src %>/img',
-        src: '**/*.{png,jpg,svg}',
-        dest: '<%= config.dist %>/img',
-        verbose: true
-      },
-      js: {
-        expand: true,
-        cwd: '<%= config.src %>/js',
-        src: '**/*',
-        dest: '<%= config.dist %>/js',
-        verbose: true
-      },
-      vendor: {
-        expand: true,
-        cwd: '<%= config.src %>/vendor',
-        src: '**/*',
-        dest: '<%= config.dist %>/vendor',
-        verbose: true
-      },
-    },
+        connect: {
+            options: {
+                port: 9000,
+                livereload: 35729,
+                // change this to '0.0.0.0' to access the server from outside
+                hostname: 'localhost'
+            },
+            livereload: {
+                options: {
+                    open: true,
+                    base: [
+                        '<%= config.dist %>'
+                    ]
+                }
+            }
+        },
 
-    // Before generating any new files,
-    // remove any previously-created files.
-    clean: ['<%= config.dist %>/**/*.{hbs}'],
+        assemble: {
+            pages: {
+                options: {
+                    flatten: true,
+                    assets: '<%= config.dist %>',
+                    layout: '<%= config.src %>/templates/layouts/default.hbs',
+                    data: '<%= config.src %>/data/*.{json,yml}',
+                    partials: '<%= config.src %>/templates/partials/*.hbs',
+                    plugins: ['assemble-contrib-permalinks', 'assemble-contrib-sitemap'],
+                },
+                files: {
+                    '<%= config.dist %>/': ['<%= config.src %>/templates/pages/*.hbs']
+                }
+            }
+        },
 
-    jsbeautifier: {
-      files: [
-        'src/js/**/*.js',
-        'Gruntfile.js'
-      ],
-      options: {
-        js: {
-          indentChar: ' ',
-          indentLevel: 0,
-          indentSize: 2,
-          indentWithTabs: false
-        }
-      }
-    },
+        htmlmin: { // Task
+            dist: { // Target
+                options: { // Target options
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: { // Dictionary of files
+                    'dist/index.html': 'dist/index.html',
+                }
+            },
+        },
 
-    // Make sure code styles are up to par and there are no obvious mistakes
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc'
-      },
-      all: [
-        '<%= config.src %>/js/**/*.js',
-        '!<%= config.src %>/js/lib/**/*.js',
-        'Gruntfile.js'
-      ]
-    },
-  });
+        // Task for syncing content of img catalogue content to dest
+        sync: {
+            img: {
+                expand: true,
+                cwd: '<%= config.src %>/img',
+                src: '**/*.{png,jpg,svg}',
+                dest: '<%= config.dist %>/img',
+                verbose: true
+            },
+            js: {
+                expand: true,
+                cwd: '<%= config.src %>/js',
+                src: '**/*',
+                dest: '<%= config.dist %>/js',
+                verbose: true
+            },
+            vendor: {
+                expand: true,
+                cwd: '<%= config.src %>/vendor',
+                src: '**/*',
+                dest: '<%= config.dist %>/vendor',
+                verbose: true
+            },
+        },
 
-  grunt.registerTask('server', [
-    'clean',
-    'assemble',
-    'sync:img',
-    'sync:js',
-    'sync:vendor',
-    'compass:server',
-    'connect:livereload',
-    'watch'
-  ]);
+        // Before generating any new files,
+        // remove any previously-created files.
+        clean: ['<%= config.dist %>/**/*.{hbs}'],
 
-  grunt.registerTask('build', [
-    'clean',
-    'assemble'
-  ]);
+        jsbeautifier: {
+            files: [
+                'src/js/**/*.js',
+                'Gruntfile.js'
+            ],
+            options: {
+                js: {
+                    indentChar: ' ',
+                    indentLevel: 0,
+                    indentSize: 4,
+                    indentWithTabs: false
+                }
+            }
+        },
 
-  grunt.registerTask('default', [
-    'build'
-  ]);
+        // Make sure code styles are up to par and there are no obvious mistakes
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc'
+            },
+            all: [
+                '<%= config.src %>/js/**/*.js',
+                '!<%= config.src %>/js/lib/**/*.js',
+                'Gruntfile.js'
+            ]
+        },
+    });
+
+    grunt.registerTask('server', [
+        'clean',
+        'assemble',
+        'sync:img',
+        'sync:js',
+        'sync:vendor',
+        'compass:server',
+        'connect:livereload',
+        'watch'
+    ]);
+
+    grunt.registerTask('build', [
+        'clean',
+        'assemble'
+    ]);
+
+    grunt.registerTask('default', [
+        'build'
+    ]);
 };
--- src/js/jreject.js
+++ src/js/jreject.js
@@ -1,126 +1,126 @@
 'use strict';
 
 (function($) {
-  $(function() {
-    var options = {
-      reject: { // Rejection flags for specific browsers
-        msie5: true,
-        msie6: true,
-        msie7: true,
-        msie8: true, // Covers MSIE 5-7 (Blocked by default)
-        firefox1: true,
-        firefox2: true // Covers FF 1-2
-        /*
-         * Possibilities are endless...
-         *
-         * // MSIE Flags (Global, 5-8)
-         * msie, msie5, msie6, msie7, msie8,
-         * // Firefox Flags (Global, 1-3)
-         * firefox, firefox1, firefox2, firefox3,
-         * // Konqueror Flags (Global, 1-3)
-         * konqueror, konqueror1, konqueror2, konqueror3,
-         * // Chrome Flags (Global, 1-4)
-         * chrome, chrome1, chrome2, chrome3, chrome4,
-         * // Safari Flags (Global, 1-4)
-         * safari, safari2, safari3, safari4,
-         * // Opera Flags (Global, 7-10)
-         * opera, opera7, opera8, opera9, opera10,
-         * // Rendering Engines (Gecko, Webkit, Trident, KHTML, Presto)
-         * gecko, webkit, trident, khtml, presto,
-         * // Operating Systems (Win, Mac, Linux, Solaris, iPhone)
-         * win, mac, linux, solaris, iphone,
-         * unknown // Unknown covers everything else
-         */
-      },
-      // What browsers to display and their order
-      display: ['chrome', 'firefox', 'safari', 'opera', 'gcf', 'msie'],
-      browserShow: true, // Should the browser options be shown?
-      browserInfo: { // Settings for which browsers to display
-        firefox: {
-          text: 'Mozilla Firefox', // Text below the icon
-          url: 'http://www.mozilla.com/firefox/' // URL For icon/text link
-        },
-        chrome: {
-          text: 'Google Chrome',
-          url: 'http://www.google.com/chrome/'
-        },
-        safari: {
-          text: 'Safari',
-          url: 'http://www.apple.com/safari/download/'
-        },
-        opera: {
-          text: 'Opera',
-          url: 'http://www.opera.com/download/'
-        },
-        msie: {
-          text: 'Internet Explorer',
-          url: 'http://www.microsoft.com/windows/Internet-explorer/'
-        },
-        gcf: {
-          text: 'Google Chrome Frame',
-          url: 'http://code.google.com/chrome/chromeframe/',
-          // This browser option will only be displayed for MSIE
-          allow: {
-            all: false,
-            msie: true
-          }
-        }
-      },
+    $(function() {
+        var options = {
+            reject: { // Rejection flags for specific browsers
+                msie5: true,
+                msie6: true,
+                msie7: true,
+                msie8: true, // Covers MSIE 5-7 (Blocked by default)
+                firefox1: true,
+                firefox2: true // Covers FF 1-2
+                /*
+                 * Possibilities are endless...
+                 *
+                 * // MSIE Flags (Global, 5-8)
+                 * msie, msie5, msie6, msie7, msie8,
+                 * // Firefox Flags (Global, 1-3)
+                 * firefox, firefox1, firefox2, firefox3,
+                 * // Konqueror Flags (Global, 1-3)
+                 * konqueror, konqueror1, konqueror2, konqueror3,
+                 * // Chrome Flags (Global, 1-4)
+                 * chrome, chrome1, chrome2, chrome3, chrome4,
+                 * // Safari Flags (Global, 1-4)
+                 * safari, safari2, safari3, safari4,
+                 * // Opera Flags (Global, 7-10)
+                 * opera, opera7, opera8, opera9, opera10,
+                 * // Rendering Engines (Gecko, Webkit, Trident, KHTML, Presto)
+                 * gecko, webkit, trident, khtml, presto,
+                 * // Operating Systems (Win, Mac, Linux, Solaris, iPhone)
+                 * win, mac, linux, solaris, iphone,
+                 * unknown // Unknown covers everything else
+                 */
+            },
+            // What browsers to display and their order
+            display: ['chrome', 'firefox', 'safari', 'opera', 'gcf', 'msie'],
+            browserShow: true, // Should the browser options be shown?
+            browserInfo: { // Settings for which browsers to display
+                firefox: {
+                    text: 'Mozilla Firefox', // Text below the icon
+                    url: 'http://www.mozilla.com/firefox/' // URL For icon/text link
+                },
+                chrome: {
+                    text: 'Google Chrome',
+                    url: 'http://www.google.com/chrome/'
+                },
+                safari: {
+                    text: 'Safari',
+                    url: 'http://www.apple.com/safari/download/'
+                },
+                opera: {
+                    text: 'Opera',
+                    url: 'http://www.opera.com/download/'
+                },
+                msie: {
+                    text: 'Internet Explorer',
+                    url: 'http://www.microsoft.com/windows/Internet-explorer/'
+                },
+                gcf: {
+                    text: 'Google Chrome Frame',
+                    url: 'http://code.google.com/chrome/chromeframe/',
+                    // This browser option will only be displayed for MSIE
+                    allow: {
+                        all: false,
+                        msie: true
+                    }
+                }
+            },
 
-      // Header of pop-up window
-      header: 'Czy wiesz, że Twoja przeglądarka internetowa jest nieaktualna?',
-      // Paragraph 1
-      paragraph1: 'Twoja przeglądarka jest nieaktualna, i może być ' +
-        'nie kompatybilna z naszą stroną. Zalecamy aktualizację ' +
-        'przeglądarki. Listę najpopularniejszych przeglądarek internetowych ' +
-        'możesz znaleźć poniżej.',
-      // Paragraph 2
-      paragraph2: 'Wystarczy kliknąć ikonę do przejścia do strony pobierania.',
-      close: true, // Allow closing of window
-      // Message displayed below closing link
-      closeMessage: 'Zamykając to okno bądz świadom że działanie tej strony może być nieprawidłowe.',
-      closeLink: 'Zamknij okno', // Text for closing link
-      closeURL: '#', // Close URL
-      closeESC: true, // Allow closing of window with esc key
+            // Header of pop-up window
+            header: 'Czy wiesz, że Twoja przeglądarka internetowa jest nieaktualna?',
+            // Paragraph 1
+            paragraph1: 'Twoja przeglądarka jest nieaktualna, i może być ' +
+                'nie kompatybilna z naszą stroną. Zalecamy aktualizację ' +
+                'przeglądarki. Listę najpopularniejszych przeglądarek internetowych ' +
+                'możesz znaleźć poniżej.',
+            // Paragraph 2
+            paragraph2: 'Wystarczy kliknąć ikonę do przejścia do strony pobierania.',
+            close: true, // Allow closing of window
+            // Message displayed below closing link
+            closeMessage: 'Zamykając to okno bądz świadom że działanie tej strony może być nieprawidłowe.',
+            closeLink: 'Zamknij okno', // Text for closing link
+            closeURL: '#', // Close URL
+            closeESC: true, // Allow closing of window with esc key
 
-      // If cookies should be used to remmember if the window was closed
-      // See cookieSettings for more options
-      closeCookie: false,
-      // Cookie settings are only used if closeCookie is true
-      cookieSettings: {
-        // Path for the cookie to be saved on
-        // Should be root domain in most cases
-        path: '/',
-        // Expiration Date (in seconds)
-        // 0 (default) means it ends with the current session
-        expires: 0
-      },
+            // If cookies should be used to remmember if the window was closed
+            // See cookieSettings for more options
+            closeCookie: false,
+            // Cookie settings are only used if closeCookie is true
+            cookieSettings: {
+                // Path for the cookie to be saved on
+                // Should be root domain in most cases
+                path: '/',
+                // Expiration Date (in seconds)
+                // 0 (default) means it ends with the current session
+                expires: 0
+            },
 
-      imagePath: getScriptURL() + '/../../vendor/jReject/images/', // Path where images are located
-      overlayBgColor: '#000', // Background color for overlay
-      overlayOpacity: 0.8, // Background transparency (0-1)
+            imagePath: getScriptURL() + '/../../vendor/jReject/images/', // Path where images are located
+            overlayBgColor: '#000', // Background color for overlay
+            overlayOpacity: 0.8, // Background transparency (0-1)
 
-      // Fade in time on open ('slow','medium','fast' or integer in ms)
-      fadeInTime: 'fast',
-      // Fade out time on close ('slow','medium','fast' or integer in ms)
-      fadeOutTime: 'fast',
+            // Fade in time on open ('slow','medium','fast' or integer in ms)
+            fadeInTime: 'fast',
+            // Fade out time on close ('slow','medium','fast' or integer in ms)
+            fadeOutTime: 'fast',
 
-      // Google Analytics Link Tracking (Optional)
-      // Set to true to enable
-      // Note: Analytics tracking code must be added separately
-      analytics: false
-    };
+            // Google Analytics Link Tracking (Optional)
+            // Set to true to enable
+            // Note: Analytics tracking code must be added separately
+            analytics: false
+        };
 
-    $.reject(options);
-  });
+        $.reject(options);
+    });
 
-  var getScriptURL = (function() {
-    var scripts = document.getElementsByTagName('script');
-    var index = scripts.length - 1;
-    var myScript = scripts[index];
-    return function() {
-      return myScript.src;
-    };
-  })();
+    var getScriptURL = (function() {
+        var scripts = document.getElementsByTagName('script');
+        var index = scripts.length - 1;
+        var myScript = scripts[index];
+        return function() {
+            return myScript.src;
+        };
+    })();
 
 })(jQuery);
